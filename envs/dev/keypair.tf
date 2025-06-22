61d1d1a393fbde552aab0e0a88c9257d0450352e
Hieu Do <hieu.do@aeris.net>
2025-06-22 16:08:56 +0700

    clean

--- envs/dev/keypair.tf
+++ envs/dev/keypair.tf
@@ -1,6 +1,3 @@
-provider "aws" {
-  region = var.region
-}
 
 resource "tls_private_key" "ssh_key_dev" {
   algorithm = "RSA"
@@ -18,31 +15,3 @@ resource "local_file" "private_key_pem_dev" {
   file_permission = "0400"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "cheap_ec2" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.generated_key_dev.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "cheap-ec2"
-  }
-}
