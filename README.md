867b796411636d4ef7e9ddfaed680efb94d527da
Hieu Do <hieu.do@aeris.net>
2025-06-23 14:08:26 +0700

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # terraform-ansible
 # terraform-ansible
+# Triển khai hạ tầng và cấu hình server bằng Terraform & Ansible
+
+Dự án này giúp bạn triển khai hạ tầng EC2, RDS... bằng Terraform, sau đó cấu hình các máy chủ bằng Ansible một cách tự động.
+
+---
+
+## 🧱 Bước 1: Khởi tạo hạ tầng bằng Terraform
+
+### ➤ Thực hiện
+
+
+```bash
+cd envs/prod
+
+terraform apply -var-file="terraform.tfvars" -auto-approve && ./generate_inventory.sh
+``` 
+## 🧱 Bước 2: Khởi tạo môi trường
+
+### ➤ Thực hiện
+```bash
+cd ../..   # trở về thư mục gốc nếu đang ở envs/prod
+
+ansible-playbook \
+  -i ansible/envs/prod/inventory.ini \
+  -u ubuntu \
+  ansible/bootstrap.yml
+``` 
---
+++ envs/prod/generate_inventory.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+APP_IP=$(terraform output -raw prod_api_private_ip)
+FRONTEND_IP=$(terraform output -raw prod_web_public_ip)
+BASTION_IP=$(terraform output -raw prod_bastion_public_ip)
+
+TARGET_PATH="../../ansible/envs/prod/inventory.ini"
+mkdir -p "$(dirname "$TARGET_PATH")"
+
+cat <<EOF > "$TARGET_PATH"
+[app]
+$APP_IP  # private IP của EC2
+
+[frontend]
+$FRONTEND_IP  # public IP của EC2 frontend
+
+[app:vars]
+ansible_user=ubuntu
+ansible_ssh_private_key_file=keys/prod-key.pem
+ansible_ssh_common_args='-o ProxyCommand="ssh -i keys/prod-key.pem -W %h:%p ubuntu@$BASTION_IP" -o StrictHostKeyChecking=no'
+
+[frontend:vars]
+ansible_user=ubuntu
+ansible_ssh_private_key_file=keys/prod-key.pem
+ansible_ssh_common_args='-o StrictHostKeyChecking=no'
+EOF
+
+echo "✔️ inventory.ini đã được tạo tại $TARGET_PATH"
